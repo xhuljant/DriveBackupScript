f58e9041c78da2034d66781906e7c5d122f25166
xhuljant <xhuljant@gmail.com>
2026-08-18 16:45:08 -0400

    update

--- StartBackupBatFile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\xhulj\Desktop\MediaOrganizerScript\FDriveBackupScript\DriveBackupScript.py"
-python FDriveBackupScript.py
-echo.
-echo Backup completed! Check backup_log.txt for details.
